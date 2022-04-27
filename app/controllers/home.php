fe1ad5cb8cd49eb847d6381616fdcf231793e660
anasjabellah <jablcodiing@gmail.com>
2022-04-27 23:47:30 +0000

    first commit

---
+++ app/controllers/home.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Home extends Controller{
+
+
+    public function index($name = ''){
+
+        $user = $this->model('User');
+        $user->name = $name;
+        echo $user->name;
+
+    }
+
+}
